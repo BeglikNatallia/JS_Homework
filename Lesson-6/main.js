6dd46cd995c240e3508b4fb3498da8dc42fcb59b
Natasha Beglik <natasha-1909@yandex.ru>
2019-06-19 22:43:43 +0300

    Homework 6 - modified

--- Lesson-6/main.js
+++ Lesson-6/main.js
@@ -22,7 +22,14 @@ function Cat(name) {
 
     var foodAmount = 50;
 
-    this.setDailyNorm = function (amount) {
+    function formatFoodAmount() {
+        return foodAmount + 'гр.';
+    }
+
+    this.DailyNorm = function (amount) {
+
+        if (!arguments.length) return foodAmount;
+
         if (amount < 50) {
             alert('Насыпьте больше корма, котик голодный!')
         }
@@ -34,28 +41,25 @@ function Cat(name) {
         foodAmount = amount;
     };
 
-    this.getDailyNorm = function () {
-        return foodAmount + 'гр.';
-    };
 
     this.feed = function() {
-        console.log('Насыпаем в миску ' + this.getDailyNorm() + ' корма.');
+        console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма.');
     };
 }
 
 var barsik = new Cat('Барсик');
 
-console.log(barsik.setDailyNorm());
+console.log(barsik.DailyNorm(25));
 
-console.log(barsik.setDailyNorm(25));
+console.log(barsik.DailyNorm(600));
 
-console.log(barsik.setDailyNorm(600));
-
-console.log(barsik.setDailyNorm(300));
+console.log(barsik.DailyNorm(300));
 
 console.log(barsik.feed());
 
-console.log(barsik.getDailyNorm());
+console.log(barsik.DailyNorm());
+
+
 
 
 
