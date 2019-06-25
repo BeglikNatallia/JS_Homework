9dbf8571487a1afa6f7eb2372eb0bfb817805cb1
Natasha Beglik <natasha-1909@yandex.ru>
2019-06-26 00:21:46 +0300

    Homework 8

---
+++ Lesson-8/main.js
@@ -0,0 +1,56 @@
+function Animal(name) {
+    var foodAmount = 50;
+
+    function formatFoodAmount() {
+        return foodAmount + 'гр.';
+    }
+
+    this.dailyNorm = function(amount) {
+        if (!arguments.length) return formatFoodAmount();
+
+        if (amount < 50 || amount > 500) {
+            return 'Недопустимое количество корма.';
+        }
+
+        foodAmount = amount;
+    };
+
+    this.name = name;
+
+    var self = this;
+
+    this.animalFeed = function() {
+        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
+    };
+}
+
+function Cat(name) {
+    Animal.apply(this, arguments);
+
+    var parentFeed = this.animalFeed;
+
+    this.feed = function() {
+        parentFeed();
+        console.log('Кот доволен ^_^');
+        return this;
+    };
+
+    this.stroke = function() {
+        console.log('Гладим кота');
+        return this;
+    }
+}
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.name);
+
+console.log(barsik.dailyNorm());
+console.log(barsik.feed());
+
+console.log(barsik.dailyNorm(300));
+console.log(barsik.feed());
+
+console.log(barsik.stroke());
+
+barsik.feed() .stroke() .feed() .feed() .stroke();
